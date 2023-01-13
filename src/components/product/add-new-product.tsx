8bdfaea57663e60ef9ff4f7ca2b4eec4e7994a11
lecon20122 <lecon20122@gmail.com>
2023-01-13 12:56:01 +0200

    working on dashboard

--- src/components/product/add-new-product.tsx
+++ src/components/product/add-new-product.tsx
@@ -21,30 +21,31 @@ export default function CreateProductForm() {
 
     return (
         <div className="overflow-hidden bg-white mx-auto rounded-lg w-full sm:w-96 md:w-450px border border-gray-300 py-5 px-5 sm:px-8">
+            <h1 className="text-center text-black">Create Product</h1>
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 className="py-5 w-full mx-auto flex flex-col justify-center"
                 noValidate
             >
                 <div className="flex flex-col space-y-4 lg:space-y-5">
-                        <Input
-                            labelKey="forms:label-title-en"
-                            {...register("en", {
-                                required: "forms:full-name-required",
-                            })}
-                            errorKey={errors.en?.message}
-                            variant="solid"
-                            className="w-full "
-                        />
-                        <Input
-                            labelKey="forms:label-title-ar"
-                            {...register("ar", {
-                                required: "forms:full-name-required",
-                            })}
-                            errorKey={errors.ar?.message}
-                            variant="solid"
-                            className="w-full"
-                        />
+                    <Input
+                        labelKey="forms:label-title-en"
+                        {...register("en", {
+                            required: "forms:full-name-required",
+                        })}
+                        errorKey={errors.en?.message}
+                        variant="solid"
+                        className="w-full "
+                    />
+                    <Input
+                        labelKey="forms:label-title-ar"
+                        {...register("ar", {
+                            required: "forms:full-name-required",
+                        })}
+                        errorKey={errors.ar?.message}
+                        variant="solid"
+                        className="w-full"
+                    />
                     <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0">
                         <Input
                             labelKey="forms:label-price"
